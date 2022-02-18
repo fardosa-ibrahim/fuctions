1fc3e78cfece5edff86b319924c2600946f98d35
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-02-18 19:52:33 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    Name()
+    add(32,13)
+    sum(12,20,32,13)
+    intresting()
+}
+fun Name(){
+    var name="fardosa"
+    println("Hello "+name)
+}
+fun add(a: Int, b: Int){
+    var result=a%b
+    println(result)
+}
+fun sum(a: Int, b: Int, c: Int, d: Int){
+    var result=a+b+c+d
+    println(result)
+}
+fun intresting(){
+    println("I can dance")
+}
